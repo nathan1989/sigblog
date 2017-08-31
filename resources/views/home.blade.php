795eb8545797e8c1394b0be0cc305164e5637cd8
nathan <nathan@goodfolk.co.nz>
2017-08-31 16:15:25 +1200

    homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+{{--
+  Template Name: Home Template
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+  @while(have_posts()) @php(the_post())
+    @include('partials.page-header')
+    @include('partials.content-page')
+    <h2>Latest post</h2>
+    <?php
+      $args = array( 'numberposts' => '1' );
+      $recent_posts = wp_get_recent_posts( $args );
+      foreach( $recent_posts as $recent ){
+        echo '<h1><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a> </h1> ';
+        echo '<div>' . $recent["post_content"] . '</div>';
+      }
+      wp_reset_query();
+    ?>
+  @endwhile
+@endsection
