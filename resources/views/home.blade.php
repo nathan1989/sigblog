dfaaeb1a02e18cceac09f2910e4c5128c3cd2b56
nathan <nathan@goodfolk.co.nz>
2017-09-01 15:12:36 +1200

    home and blog

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,14 +9,14 @@
     @include('partials.page-header')
     @include('partials.content-page')
     <h2>Latest post</h2>
-    <?php
-      $args = array( 'numberposts' => '1' );
-      $recent_posts = wp_get_recent_posts( $args );
-      foreach( $recent_posts as $recent ){
-        echo '<h1><a href="' . get_permalink($recent["ID"]) . '">' .   $recent["post_title"].'</a> </h1> ';
-        echo '<div>' . $recent["post_content"] . '</div>';
-      }
-      wp_reset_query();
-    ?>
+    <?php 
+		$wp_query = new WP_Query(); $wp_query->query('posts_per_page=1');
+		while ($wp_query->have_posts()) : $wp_query->the_post(); ?>
+
+      @include('partials/content')
+
+		<?php endwhile; ?>
+
+		<?php wp_reset_postdata(); ?>
   @endwhile
 @endsection
