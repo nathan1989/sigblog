5e5d2e0e2d4e06a163663914bed51bd9cb685021
nathan <nathan@goodfolk.co.nz>
2017-09-01 16:04:46 +1200

    about page

---
+++ resources/views/about.blade.php
@@ -0,0 +1,23 @@
+{{--
+  Template Name: About Template
+--}}
+
+@extends('layouts.app')
+
+@section('content')
+  @while(have_posts()) @php(the_post()) 
+    <div class="about-wrapper">
+      <?php
+      $image = get_field('about-image');
+      if( !empty($image) ): ?>
+      <div class="image-wrap">
+        <img src="<?php echo $image['sizes'][ 'about-cropped-image' ]; ?>" alt="<?php echo $image['alt']; ?>" />
+      </div>
+      <?php endif; ?>
+      <div class="content">
+        @include('partials.page-header')   
+        @include('partials.content-page')
+      </div>
+    </div>
+  @endwhile
+@endsection
