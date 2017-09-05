f7d92bc12349a11d53696ea98f8441114ac7e54b
nathan <nathan@goodfolk.co.nz>
2017-09-06 10:04:12 +1200

    header

--- resources/views/partials/page-header.blade.php
+++ resources/views/partials/page-header.blade.php
@@ -1,9 +1,5 @@
 <div class="page-header">
-  @if(is_front_page())
-    <h1>Latest post</h1>
-  @else
-    <h1>{!! App\title() !!}</h1>
-  @endif
+  <h1>{!! App\title() !!}</h1>
   @if (category_description())
   {!! category_description() !!}
   @endif
