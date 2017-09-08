74bde4dbb2d1dfb554fbaca18b749cf0c4522f60
Nathan Smith <nathansmithdeveloper@gmail.com>
2017-09-08 20:51:30 +1200

    client changes

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -3,9 +3,6 @@ export default {
     // JavaScript to be fired on all pages
     const navItem = document.querySelectorAll('.menu-item-has-children')
     function showMenu(){
-      if (is_touch_device()){
-        // e.preventDefault()
-      }
       this.classList.add('trigger-enter')
       setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150)
     }
@@ -16,6 +13,15 @@ export default {
     navItem.forEach(item => item.addEventListener('mouseenter', showMenu))
     navItem.forEach(item => item.addEventListener('mouseleave', hideMenu))
 
+    if(is_touch_device()){
+      navItem.forEach(item => item.addEventListener('click', function(e){
+        e.preventDefault()
+        if(this.classList.contains('trigger-enter')){
+          this.classList.remove('trigger-enter')
+        }
+      }))
+    }
+
 
     function is_touch_device() {
       // works on most browsers, IE10/11 and Surface
@@ -25,7 +31,6 @@ export default {
         return false
       }
     }
-
     console.log(is_touch_device())
   },
   finalize() {
