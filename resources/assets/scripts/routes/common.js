1da8886a6e5ecef7028b1e6c454d4f7b6be506db
nathan <nathan@goodfolk.co.nz>
2017-09-08 13:39:48 +1200

    started on touch detection

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -3,6 +3,9 @@ export default {
     // JavaScript to be fired on all pages
     const navItem = document.querySelectorAll('.menu-item-has-children')
     function showMenu(){
+      if (is_touch_device()){
+        // e.preventDefault()
+      }
       this.classList.add('trigger-enter')
       setTimeout(() => this.classList.contains('trigger-enter') && this.classList.add('trigger-enter-active'), 150)
     }
@@ -12,6 +15,18 @@ export default {
 
     navItem.forEach(item => item.addEventListener('mouseenter', showMenu))
     navItem.forEach(item => item.addEventListener('mouseleave', hideMenu))
+
+
+    function is_touch_device() {
+      // works on most browsers, IE10/11 and Surface
+      if ('ontouchstart' in window || navigator.maxTouchPoints) {
+        return true
+      } else {
+        return false
+      }
+    }
+
+    console.log(is_touch_device())
   },
   finalize() {
     // JavaScript to be fired on all pages, after page specific JS is fired
