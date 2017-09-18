b3d14407d995b29173dc210b203b308df862eb1f
Nathan Smith <nathansmithdeveloper@gmail.com>
2017-09-18 20:59:19 +1200

    client updates

--- resources/views/partials/content-single.blade.php
+++ resources/views/partials/content-single.blade.php
@@ -3,11 +3,6 @@
     <h1 class="entry-title">{{ get_the_title() }}</h1>
     @include('partials/entry-meta')
   </header>
-  @if (has_post_thumbnail())
-  <div class="image-wrap">
-    @php(the_post_thumbnail('blog-large-image'))
-  </div>
-  @endif
   <div class="entry-content">
     @php(the_content())
   </div>
